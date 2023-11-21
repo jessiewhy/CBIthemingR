2b4a70f3d68c5d48010db2fd3f33e77475d8d2f6
Jessie Huang <32107743+jessiewhy@users.noreply.github.com>
2023-11-20 20:02:55 -0500

    theming colors added

---
+++ R/cbi_geom_defaults.R
@@ -0,0 +1,23 @@
+#' @importFrom ggplot2 update_geom_defaults .pt
+
+cbi_geom_defaults <- function() {
+  # Setting consistent defaults for individual geoms
+
+  # TODO: update_geom_defaults() could be deprecated in a
+  # future ggplot2
+  # release (see https://github.com/tidyverse/ggplot2/pull/2749)
+  # when this happens, code below will have to be replaced
+  update_geom_defaults('point',
+                       list(colour = PHC6099FinalProject::cbi_base_lightblue,
+                            size = 6 / .pt))
+  update_geom_defaults('line',
+                       list(colour = PHC6099FinalProject::cbi_base_lightblue,
+                            size = 3 / .pt))
+  update_geom_defaults('text',
+                       list(colour = PHC6099FinalProject::cbi_base_black,
+                            size = 18 / .pt))
+  update_geom_defaults('label',
+                       list(size = 18 / .pt,
+                            fill = PHC6099FinalProject::cbi_base_white,
+                            colour = PHC6099FinalProject::cbi_base_lightblue))
+}
